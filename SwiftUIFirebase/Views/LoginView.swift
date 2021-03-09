1cdbd281502201d64f771dccbc6839e8983a4ba5
Alfredo Luco <aluco@Alfredos-MacBook-Pro.local>
2021-03-09 12:00:08 -0300

    loading

--- SwiftUIFirebase/Views/LoginView.swift
+++ SwiftUIFirebase/Views/LoginView.swift
@@ -11,6 +11,7 @@ import AuthenticationServices
 struct LoginView: View {
     
     @ObservedObject var loginViewModel: LoginViewModel
+    @State var loading: Bool = false
     
     init() {
         loginViewModel = LoginViewModel()
@@ -20,10 +21,14 @@ struct LoginView: View {
     var body: some View {
         VStack {
             Spacer()
+            if loading {
+                ProgressView()
+            }
             SignInWithAppleButton(.signIn) { (request) in
                 request.nonce = self.loginViewModel.nonce.sha256()
                 request.requestedScopes = [.email,.fullName]
             } onCompletion: { result in
+                self.loading.toggle()
                 self.loginViewModel.loginWithApple(result)
             }.signInWithAppleButtonStyle(.black)
             .cornerRadius(8.0)
@@ -31,15 +36,19 @@ struct LoginView: View {
             .frame(height: 90.0)
 
             FBButton(action: {
+                self.loading.toggle()
                 self.loginViewModel.loginWithFacebook()
             }).frame(height: 60, alignment: .center)
             AnnonymousButton(action: {
+                self.loading.toggle()
                 self.loginViewModel.loginAnnonymously()
             })
             .frame( height: 85, alignment: .center)
         }
         .alert(isPresented: self.$loginViewModel.showingAlert, content: {
-            Alert(title: Text(self.loginViewModel.alertTitle), message: Text(self.loginViewModel.alertMessage), dismissButton: Alert.Button.default(Text("OK")))
+            Alert(title: Text(self.loginViewModel.alertTitle), message: Text(self.loginViewModel.alertMessage), dismissButton: Alert.Button.default(Text("OK"),action: {
+                self.loading = false
+            }))
         })
     }
 }
